0e1ccd70a71c34b809fe004aaf8d9f84887e2883
Ltae <ltae36@gmail.com>
2024-06-28 18:34:26 +0900

    0628

--- Assets/Scripts/E_Child.cs
+++ Assets/Scripts/E_Child.cs
@@ -8,12 +8,12 @@ public class E_Child : MonoBehaviour
     private bool isStop = true;
     float currentTime = 0;
     float downTime = 1.5f;
+    public GameObject player;
+    Vector3 dir = new Vector3(0, 0, 1);
 
-    //넘어지는 애니메이션 재생
 
     void Start()
     {
-        // 애니메이터 컴포넌트를 가져온다
     }
 
     void Update()
@@ -21,9 +21,9 @@ public class E_Child : MonoBehaviour
         if(isStop == true)
         {
             // 이동 공식 : p = p0 +vt
-            transform.position += new Vector3(0, 0, 1) * moveSpeed * Time.deltaTime;
-
             
+            transform.position += dir * moveSpeed * Time.deltaTime;
+                        
             // 무작위 방향으로 곡선을 그리며 뛰어다닌다.
 
             // 길을 벗어나거나 나무박스에 부딪힐 것 같다면, 방향을 변경한다.
@@ -44,7 +44,14 @@ public class E_Child : MonoBehaviour
     // 넘어 지면 잠깐 멈췄다가 일어나서 다시 뛰어다닌다.
     private void OnTriggerEnter(Collider other)
     {
-        isStop = false;
+        if(other == player) 
+        {
+            isStop = false;
+        }
+        else 
+        {
+            dir = new Vector3(1, 0, 0);
+        }
     }
 
 
