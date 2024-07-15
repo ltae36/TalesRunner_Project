da34335e38b6b132b2bae6ce1834b0795cda33ef
cob1887 <maincore449@hotmail.com>
2024-07-15 18:28:26 +0900

    ㅗㅗ

--- Assets/CharacterScript/LobbyManager.cs
+++ Assets/CharacterScript/LobbyManager.cs
@@ -8,6 +8,9 @@ public class LobbyManager : MonoBehaviour
 {
 
     public AudioSource auidoSource;
+    //하이어라키에 있는 버튼 설정
+    GameObject GameStart;
+
 
 
     void Start()
@@ -19,33 +22,31 @@ public class LobbyManager : MonoBehaviour
     {
         LobbyBGM();  // 시작하면 로비 bgm 시작
 
-        if (Input.anyKeyDown)     // 아무 키를 입력하aus
-        {
-            SceneManager.LoadScene("1");   // 씬 1번으로 이동
-        }
+        
+        Gamestart();
 
-        if (Input.GetKeyDown(KeyCode.Escape))   // esc를 누르면 어플리케이션 종료
-        {
-            Application.Quit();
-        }
+        Gamequit();
+
+
+
     }
 
     public void Gamestart()  // 게임 시작버튼  누를시 1번에 있는 씬으로 전환(trackScene)  ---- 게임 시작 버튼에 드래그&드롭
     {
-        SceneManager.LoadScene("1");
+        SceneManager.LoadScene("RacingTrack");
     }
 
 
-    //public void Gamequit()  //  quit 버튼을 누르면 어플리케이션 종료
-    //{
-    //    if(Input.GetButtonDown("Escape"))   
-    //        {
-    //           Application.Quit();
-    //        }
-    //}
+    public void Gamequit()  //  quit 버튼을 누르면 어플리케이션 종료
+    {
+        Application.Quit();
+
+    }
 
     public void LobbyBGM()
     {
+        auidoSource = GetComponent<AudioSource>();
+
         if(auidoSource != null && !auidoSource.isPlaying)  //로비 bgm이 null 이 아니고 플레이 중이 아니라면
         {
             auidoSource.Play();
